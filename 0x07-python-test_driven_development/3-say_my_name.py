8aa6dab60e444430370a41cc5734bb4bc5d2b71d
Amine Sehlaoui <a.sehlaoui96@gmail.com>
2023-09-05 14:58:56 -0700

    tasks done

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module 3-say_my_name """
+
+
+def say_my_name(first_name: str, last_name: str = "") -> None:
+    """ Prints "My name is [name]" """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+This is the readme file of 0x07-python-test_driven_development
+made by Amine Sehlaoui 
+Cohort 15 ALX
