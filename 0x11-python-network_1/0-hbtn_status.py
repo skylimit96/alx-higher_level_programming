e1b6f49df9189d6da4e67e75bb02094477df4596
amine <a.sehlaoui96@gmail.com>
2023-12-04 13:08:27 -0800

    updated task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
+""" Python script that fetches https://intranet.hbtn.io/status """
 
+import urllib.request
+from urllib.error import HTTPError
 
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(
+                'https://alx-intranet.hbtn.io/status'
+        ) as response:
+            html = response.read()
+            print('Body response:')
+            print("\t- type: {}".format(type(html)))
+            print("\t- content: {}".format(html))
+            print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    except HTTPError as e:
+        print("Error: {}".format(e))
